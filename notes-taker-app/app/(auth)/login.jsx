888c47e7b813c17532e6ae5e8156cc0226ea5b2c
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-05-05 18:46:48 +0530

    added loginnscreen UI

--- notes-taker-app/app/(auth)/login.jsx
+++ notes-taker-app/app/(auth)/login.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, Text, TextInput, TouchableOpacity, View} from 'react-native';
 
-const login = () => {
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = async () => {
+        alert("logged in");
+    };
   return (
-    <View>
-      <Text>login</Text>
-    </View>
+    <SafeAreaView>
+        <View>
+            <Text>Welcome back to notes taker</Text>
+        </View>
+        <View>
+            <View>
+                <Text>Email</Text>
+                <TextInput
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={setEmail}
+                />
+            </View>
+            <View>
+                <Text>Password</Text>
+                <TextInput
+                    placeholder="Password"
+                    secureTextEntry
+                    value={password}
+                    onChangeText={setPassword}
+                />
+            </View>
+            <View>
+                <TouchableOpacity onPress={login}>
+                    <Text>Login</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    </SafeAreaView>
   )
 }
 
-export default login;
+export default Login;
