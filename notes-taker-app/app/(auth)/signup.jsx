6237d8a5c239d5d2ce5bc69033d2ed826ee1b072
SudeepBhandari02 <sudeepdbhandari@gmail.com>
2025-05-05 18:56:14 +0530

    added registrationscreen UI

--- notes-taker-app/app/(auth)/signup.jsx
+++ notes-taker-app/app/(auth)/signup.jsx
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, TextInput, TouchableOpacity, SafeAreaView} from 'react-native'
+import React, {useState} from 'react'
 
-const signup = () => {
+const Signup = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const login = async () => {
+        alert("logged in");
+    };
   return (
-    <View>
-      <Text>signup</Text>
-    </View>
+      <SafeAreaView>
+          <View>
+              <Text>Welcome to notes taker</Text>
+          </View>
+          <View>
+              <View>
+                  <Text>Email</Text>
+                  <TextInput
+                      placeholder="Email"
+                      value={email}
+                      onChangeText={setEmail}
+                  />
+              </View>
+              <View>
+                  <Text>Password</Text>
+                  <TextInput
+                      placeholder="Password"
+                      secureTextEntry
+                      value={password}
+                      onChangeText={setPassword}
+                  />
+              </View>
+              <View>
+                  <TouchableOpacity onPress={login}>
+                      <Text>Login</Text>
+                  </TouchableOpacity>
+              </View>
+          </View>
+      </SafeAreaView>
   )
 }
 
-export default signup;
+export default Signup;
